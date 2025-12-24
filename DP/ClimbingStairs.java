ac2ae91df453558879816b0e2969e59aa4e035aa
Mayank Kumar <mkvasudev768@gmail.com>
2025-12-24 23:21:29 +0530

    Climbing Stairs

---
+++ DP/ClimbingStairs.java
@@ -0,0 +1,15 @@
+
+
+public class ClimbingStairs {
+    public static int ways(int n){
+        if(n==0) return 1;
+        if(n<0) return 0;
+        return ways(n-1)+ways(n-2);
+    }
+
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(" the number of ways to climb " + n + " stairs is: " + ways(n));
+        
+    }
+}
